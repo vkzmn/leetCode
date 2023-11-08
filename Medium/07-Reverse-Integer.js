1c46d2458da23c829f6a93c0aaf8472f423b5c3c
Viacheslav Kuzmin <vkuzmin.dev@gmail.com>
2023-11-07 19:42:31 -0500

    Reverse Integer

---
+++ Medium/07-Reverse-Integer.js
@@ -0,0 +1,35 @@
+// Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+// Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+// Example 1:
+
+// Input: x = 123
+// Output: 321
+// Example 2:
+
+// Input: x = -123
+// Output: -321
+// Example 3:
+
+// Input: x = 120
+// Output: 21
+
+// Constraints:
+
+// -231 <= x <= 231 - 1
+
+/**
+ * @param {number} x
+ * @return {number}
+ */
+const reverse = (x) => {
+  let result = 0;
+  let lastDigit;
+  while (x != 0) {
+    lastDigit = x % 10;
+    result = result * 10 + lastDigit;
+    x = parseInt(x / 10);
+  }
+  return result > Math.pow(2, 31) || result < Math.pow(-2, 31) ? 0 : result;
+};
