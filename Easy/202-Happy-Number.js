7eb7f52a887ed7ed8931e633c0a0e51709669e27
Viacheslav Kuzmin <vkuzmin.dev@gmail.com>
2023-11-17 18:13:01 -0500

    Happy Numbers

---
+++ Easy/202-Happy-Number.js
@@ -0,0 +1,61 @@
+// Write an algorithm to determine if a number n is happy.
+
+// A happy number is a number defined by the following process:
+
+// Starting with any positive integer, replace the number by the sum of the squares of its digits.
+// Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+// Those numbers for which this process ends in 1 are happy.
+// Return true if n is a happy number, and false if not.
+
+// Example 1:
+
+// Input: n = 19
+// Output: true
+// Explanation:
+// 12 + 92 = 82
+// 82 + 22 = 68
+// 62 + 82 = 100
+// 12 + 02 + 02 = 1
+// Example 2:
+
+// Input: n = 2
+// Output: false
+
+// Constraints:
+
+// 1 <= n <= 231 - 1
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+const squares = {
+  0: 0,
+  1: 1,
+  2: 4,
+  3: 9,
+  4: 16,
+  5: 25,
+  6: 36,
+  7: 49,
+  8: 64,
+  9: 81,
+};
+
+const isHappy = (n) => {
+  if (n < 1) return false;
+
+  let set = new Set();
+
+  while (!set.has(n)) {
+    set.add(n);
+    let s = n.toString();
+    n = 0;
+    for (let i = 0; i < s.length; ++i) {
+      n += squares[s[i]];
+    }
+    if (n == 1) return true;
+  }
+
+  return false;
+};
