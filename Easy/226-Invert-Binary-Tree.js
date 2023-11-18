c5660510631b2f9e5b34fd611cd38fa075beb514
Viacheslav Kuzmin <vkuzmin.dev@gmail.com>
2023-11-18 17:47:26 -0500

    Invert Binary Tree

---
+++ Easy/226-Invert-Binary-Tree.js
@@ -0,0 +1,42 @@
+// Given the root of a binary tree, invert the tree, and return its root.
+
+// Example 1:
+
+// Input: root = [4,2,7,1,3,6,9]
+// Output: [4,7,2,9,6,3,1]
+// Example 2:
+
+// Input: root = [2,1,3]
+// Output: [2,3,1]
+// Example 3:
+
+// Input: root = []
+// Output: []
+
+// Constraints:
+
+// The number of nodes in the tree is in the range [0, 100].
+// -100 <= Node.val <= 100
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+const invertTree = (root) => {
+  function dfs(root) {
+    if (!root) return;
+    [root.left, root.right] = [root.right, root.left];
+    dfs(root.left);
+    dfs(root.right);
+  }
+  dfs(root);
+  return root;
+};
