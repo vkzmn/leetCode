39bcce72e6d3796c7f2535b982ab5465b7a821e5
Viacheslav Kuzmin <vkuzmin.dev@gmail.com>
2023-12-04 16:45:50 -0500

    Word Search

---
+++ Medium/79-Word Search.js
@@ -0,0 +1,70 @@
+// https://leetcode.com/problems/word-search/description/
+
+// Given an m x n grid of characters board and a string word, return true if word exists in the grid.
+
+// The word can be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring. The same letter cell may not be used more than once.
+
+// Example 1:
+
+// Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
+// Output: true
+// Example 2:
+
+// Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "SEE"
+// Output: true
+// Example 3:
+
+// Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCB"
+// Output: false
+
+// Constraints:
+
+// m == board.length
+// n = board[i].length
+// 1 <= m, n <= 6
+// 1 <= word.length <= 15
+// board and word consists of only lowercase and uppercase English letters.
+
+// Follow up: Could you use search pruning to make your solution faster with a larger board?
+
+/**
+ * @param {character[][]} board
+ * @param {string} word
+ * @return {boolean}
+ */
+const exist = (board, word) => {
+  if (board.length === 0) return false;
+
+  const h = board.length;
+  const w = board[0].length;
+  const directions = [
+    [-1, 0],
+    [0, 1],
+    [1, 0],
+    [0, -1],
+  ];
+
+  const walk = (x, y, k) => {
+    if (board[x][y] !== word[k]) return false;
+    if (k === word.length - 1) return true;
+
+    board[x][y] = "*"; // mark as visited
+    for (const [dx, dy] of directions) {
+      const i = x + dx;
+      const j = y + dy;
+      if (i >= 0 && i < h && j >= 0 && j < w) {
+        if (walk(i, j, k + 1)) return true;
+      }
+    }
+    board[x][y] = word[k]; // reset
+    return false;
+  };
+
+  for (let i = 0; i < h; i++) {
+    for (let j = 0; j < w; j++) {
+      if (walk(i, j, 0)) return true;
+    }
+  }
+
+  return false;
+};
