58191cc5dde640e19dfd1c6d828aa4dc14e66e85
Viacheslav Kuzmin <vkuzmin.dev@gmail.com>
2024-01-02 15:57:16 -0500

    Valid Anagram

---
+++ Easy/241-Valid-Anagram.js
@@ -0,0 +1,49 @@
+// https://leetcode.com/problems/valid-anagram/
+
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+// Example 1:
+
+// Input: s = "anagram", t = "nagaram"
+// Output: true
+// Example 2:
+
+// Input: s = "rat", t = "car"
+// Output: false
+
+// Constraints:
+
+// 1 <= s.length, t.length <= 5 * 104
+// s and t consist of lowercase English letters.
+
+// Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+const isAnagram = (s, t) => {
+  if (s.length !== t.length) return false;
+
+  let map = new Map();
+
+  for (let char of s) {
+    if (!map.has(char)) {
+      map.set(char, 1);
+    } else {
+      map.set(char, map.get(char) + 1);
+    }
+  }
+
+  for (let char of t) {
+    if (!map.has(char) || map.get(char) === 0) {
+      return false;
+    } else {
+      map.set(char, map.get(char) - 1);
+    }
+  }
+  return true;
+};
