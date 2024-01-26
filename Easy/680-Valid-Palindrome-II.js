0500010f64132709e67cac158d4e4cc354f48c7f
Viacheslav Kuzmin <vkuzmin.dev@gmail.com>
2024-01-26 18:03:31 -0500

    Valid Palindrome II

---
+++ Easy/680-Valid-Palindrome-II.js
@@ -0,0 +1,39 @@
+// https://leetcode.com/problems/valid-palindrome-ii/
+
+// Given a string s, return true if the s can be palindrome after deleting at most one character from it.
+
+// Example 1:
+
+// Input: s = "aba"
+// Output: true
+// Example 2:
+
+// Input: s = "abca"
+// Output: true
+// Explanation: You could delete the character 'c'.
+// Example 3:
+
+// Input: s = "abc"
+// Output: false
+
+// Constraints:
+
+// 1 <= s.length <= 105
+// s consists of lowercase English letters.
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+const validPalindrome = (s, skipped = false) => {
+  for (let i = 0, j = s.length - 1; i < j; i++, j--) {
+    if (s[i] !== s[j]) {
+      if (skipped) return false;
+      return (
+        validPalindrome(s.substring(i, j), true) ||
+        validPalindrome(s.substring(i + 1, j + 1), true)
+      );
+    }
+  }
+  return true;
+};
